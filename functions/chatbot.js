b37eba3f28f308f8cb55a2f56dd6622076bd2030
tongthanaphon <tongthanaphon.tk@gmail.com>
2024-06-16 16:58:03 +0700

    update

--- functions/chatbot.js
+++ functions/chatbot.js
@@ -1,6 +1,13 @@
 const functions = require("firebase-functions");
 const { WebhookClient } = require("dialogflow-fulfillment");
 const dayjs = require("dayjs");
+const utc = require("dayjs/plugin/utc");
+const timezone = require("dayjs/plugin/timezone");
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+dayjs.tz.setDefault("Asia/Bangkok");
 
 const ledger = require("./utils/ledger");
 
@@ -11,7 +18,49 @@ exports.chatbot = functions
     const lineId =
       request.body.originalDetectIntentRequest.payload.data.source.userId;
 
-    async function ledgerManagement(agent) {
+    async function ledgerSummary(agent) {
+      const year = agent.parameters.year;
+      const month = agent.parameters.month;
+
+      if (!year || !month) {
+        await agent.add("ไม่สามารถทำรายการต่อได้ ลองดูใหม่อีกทีนะ");
+        return;
+      }
+
+      const data = await ledger.get(lineId);
+      const transactions = data?.[year]?.[month]?.transactions || [];
+
+      if (transactions.length === 0) {
+        await agent.add("ไม่พบข้อมูลการทำรายการในเดือนนี้");
+        return;
+      }
+
+      const result = transactions.reduce(
+        (acc, cur) => {
+          if (cur.type === "deposit") {
+            acc.deposit += Number(cur.amount);
+          } else if (cur.type === "withdraw") {
+            acc.withdraw += Number(cur.amount);
+          }
+
+          return acc;
+        },
+        {
+          deposit: 0,
+          withdraw: 0,
+        }
+      );
+
+      const response = [
+        `รายรับ/รายจ่าย ของ ${year} ${month} คือ `,
+        `deposit ${result.deposit} บาท`,
+        `withdraw ${result.withdraw} บาท`,
+        `รวม ${result.deposit - result.withdraw} บาท`,
+      ];
+      await agent.add(response.join("\n"));
+    }
+
+    async function ledgerTransactions(agent) {
       const amount = agent.parameters.amount;
       let type = "";
 
@@ -27,7 +76,7 @@ exports.chatbot = functions
       }
 
       const year = dayjs().year();
-      const month = dayjs().month();
+      const month = dayjs().month() + 1; // month is 0-indexed, convert to 1-indexed
 
       const data = await ledger.get(lineId);
       let payload = {};
@@ -45,19 +94,32 @@ exports.chatbot = functions
         payload = { ...data };
       }
 
-      payload[year][month].transactions.push({
-        type,
-        amount: parseFloat(amount),
-      });
-      payload.updatedAt = dayjs().unix();
+      if (!payload[year][month]) {
+        payload[year][month] = {
+          transactions: [
+            {
+              type,
+              amount: parseFloat(amount),
+            },
+          ],
+        };
+      } else {
+        payload[year][month].transactions.push({
+          type,
+          amount: parseFloat(amount),
+        });
+      }
+
+      payload.updatedAt = dayjs().tz().format();
 
       await ledger.set(lineId, payload);
-      await agent.add("บันทึกข้อมูลเรียบร้อย");
+      await agent.add("บันทึกข้อมูลเรียบร้อยแล้ว 😄");
     }
 
     let intentMap = new Map();
 
-    intentMap.set("Ledger - Management - yes", ledgerManagement);
+    intentMap.set("Ledger - Transactions - yes", ledgerTransactions);
+    intentMap.set("Ledger - Summary - yes", ledgerSummary);
 
     agent.handleRequest(intentMap);
   });
